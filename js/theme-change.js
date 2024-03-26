7a6dcfcb75daeffd494d14d01c587114eebe19f0
duydev245 <htduy2405@gmail.com>
2024-03-26 15:21:04 +0700

    ready

--- js/theme-change.js
+++ js/theme-change.js
@@ -1,22 +1,11 @@
-const btn = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem("theme");
-
-// nếu chế độ được lưu trong localStorage là "dark"
-if (currentTheme === "dark") {
-    // thì sẽ dùng .dark-theme class
-    document.body.classList.add("dark-theme");
-}
-
-btn.addEventListener("click", function () {
-    // Toggle class .dark-theme mỗi lần click
+var navbar__icon = document.getElementById("navbar__icon");
+navbar__icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
-
-    let theme = "light";
-    // Nếu body chứa class .dark-theme
     if (document.body.classList.contains("dark-theme")) {
-        // thì gán biến theme thành dark
-        theme = "dark";
+        navbar__icon.classList.remove("gg-moon");
+        navbar__icon.classList.add("gg-sun");
+    } else {
+        navbar__icon.classList.remove("gg-sun");
+        navbar__icon.classList.add("gg-moon");
     }
-    // lưu lựa chọn vào localStorage
-    localStorage.setItem("theme", theme);
-});
+};
