b81bba56906f36cb26861bdb4ff3748de84b3271
nhanDev <nhannguyen.work0608@gmail.com>
2024-03-26 19:58:39 +0700

    navbar responsive fix

--- js/theme-change.js
+++ js/theme-change.js
@@ -1,11 +1,11 @@
 var navbar__icon = document.getElementById("navbar__icon");
 navbar__icon.onclick = function () {
-    document.body.classList.toggle("dark-theme");
-    if (document.body.classList.contains("dark-theme")) {
-        navbar__icon.classList.remove("gg-moon");
-        navbar__icon.classList.add("gg-sun");
-    } else {
-        navbar__icon.classList.remove("gg-sun");
-        navbar__icon.classList.add("gg-moon");
-    }
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    navbar__icon.classList.remove("gg-moon");
+    navbar__icon.classList.add("gg-sun");
+  } else {
+    navbar__icon.classList.remove("gg-sun");
+    navbar__icon.classList.add("gg-moon");
+  }
 };
